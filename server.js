5ee194e36993cb9c83ce528a882e41dc956d4c36
Danila Kuzmuk <Da.Kuzmuk@ssu.edu.ua>
2021-02-25 11:34:52 +0200

    Test

--- server.js
+++ server.js
@@ -19,3 +19,28 @@ require('greenlock-express')
     // Get's SSL certificates magically!
     .ready(httpsWorker);
     
+    function httpsWorker(glx) {
+        var socketio = require("socket.io");
+        var io;
+    
+        // we need the raw https server
+        var server = glx.httpsServer();
+    
+        io = socketio(server);
+    
+        // Then you do your socket.io stuff
+        io.on("connection", function(socket) {
+            console.log("a user connected");
+            socket.emit("Welcome");
+    
+            socket.on("chat message", function(msg) {
+                socket.broadcast.emit("chat message", msg);
+            });
+        });
+    
+        // servers a node app that proxies requests to a localhost
+        glx.serveApp(function(req, res) {
+            res.setHeader("Content-Type", "text/html; charset=utf-8");
+            res.end("Hello, World!\n\n💚 🔒.js");
+        });
+    }
